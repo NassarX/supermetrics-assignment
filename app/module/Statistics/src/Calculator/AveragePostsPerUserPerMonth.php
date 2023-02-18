b5134e345bc0bf104e67db9c5239af3982776dcd
NassarX <mahmoud@nassarx.com>
2023-02-18 17:21:47 +0000

    feat: Implement AveragePostsPerUserPerMonth calculator class.
    Add implementation for calculating the average number of posts per user for each month.
    - `doAccumulate` method accumulates the number of posts and unique user IDs for each month.
    
    - `doCalculate` method calculates the monthly average number of posts per user and generates StatisticsTo objects to hold the results.

--- app/module/Statistics/src/Calculator/AveragePostsPerUserPerMonth.php
+++ app/module/Statistics/src/Calculator/AveragePostsPerUserPerMonth.php
@@ -12,18 +12,54 @@ class AveragePostsPerUserPerMonth extends AbstractCalculator
     protected const UNITS = 'posts';
 
     /**
-     * @inheritDoc
+     * @var array
+     */
+    private array $monthlyPosts = [];
+
+    /**
+     * Accumulate posts statics.
+     *
+     * @param SocialPostTo $postTo
+     * @return void
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // accumulate posts stats
+        $userId = $postTo->getAuthorId();
+        $monthPosts = $postTo->getDate()->format('M, Y');
+
+        // Check if an entry exists for the user else create an entry with default values
+        $this->monthlyPosts[$monthPosts]['count'] = ($this->monthlyPosts[$monthPosts]['count'] ?? 0) + 1;
+
+        // Add unique user Ids
+        if (!in_array($userId, $this->monthlyPosts[$monthPosts]['users'] ?? [], true)) {
+            $this->monthlyPosts[$monthPosts]['users'][] = $userId;
+        }
     }
 
     /**
-     * @inheritDoc
+     * Calculate Average number of posts per user for each month.
+     *
+     * @return StatisticsTo
      */
     protected function doCalculate(): StatisticsTo
     {
-        return new StatisticsTo();
+        $stats = new StatisticsTo();
+        $stats->setName($this->parameters->getStatName());
+
+        foreach ($this->monthlyPosts as $month => $monthDetails) {
+            $postsCount = $monthDetails['count'] ?? 0;
+            $postsUsersCount = count($monthDetails['users'] ?? []);
+
+            // Get user posts monthly average rounded
+            $monthlyAverage = round(($postsCount / $postsUsersCount), 0, PHP_ROUND_HALF_DOWN);
+            $child = (new StatisticsTo())
+                ->setName($this->parameters->getStatName())
+                ->setSplitPeriod($month)
+                ->setValue($monthlyAverage)
+                ->setUnits(self::UNITS);
+            $stats->addChild($child);
+        }
+
+        return $stats;
     }
 }
