dc5856fce79d2dd16f720049239ea08bae910f7d
NassarX <mahmoud@nassarx.com>
2023-02-18 17:30:06 +0000

    feat: Implement new logic for AveragePostsPerUserPerMonth.
    A different logic implementation for the AveragePostsPerUserPerMonth calculator, which calculates the average number of posts per month for each user. So it groups the posts by user and month and then calculates the average based on the total number of posts and the number of months.
    
    - `doAccumulate` method been updated to group the posts by user and month and increment the count for each user-month pair.
    - `doCalculate` method has been updated to calculate the average for each user.

--- app/module/Statistics/src/Calculator/AveragePostsPerUserPerMonth.php
+++ app/module/Statistics/src/Calculator/AveragePostsPerUserPerMonth.php
@@ -12,18 +12,55 @@ class AveragePostsPerUserPerMonth extends AbstractCalculator
     protected const UNITS = 'posts';
 
     /**
-     * @inheritDoc
+     * @var array
+     */
+    private array $userMonthlyPosts = [];
+
+    /**
+     * Accumulate posts data.
+     *
+     * @param SocialPostTo $postTo
+     * @return void
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // accumulate posts stats
+        $user     = $postTo->getAuthorName() . ' (' . $postTo->getAuthorId() . ')';
+        $postMonth  = $postTo->getDate()->format('M, Y');
+
+        // Check if an entry exists for the user else create an entry with default values
+        $this->userMonthlyPosts[$user] =
+            $this->userMonthlyPosts[$user] ?? [$postMonth => 0];
+
+        // Increment the count for the user and month
+        $this->userMonthlyPosts[$user][$postMonth] =
+            ($this->userMonthlyPosts[$user][$postMonth] ?? 0) + 1;
     }
 
     /**
-     * @inheritDoc
+     * Calculate Average number of posts per month foreach user.
+     *
+     * @return StatisticsTo
      */
     protected function doCalculate(): StatisticsTo
     {
-        return new StatisticsTo();
+        $stats = new StatisticsTo();
+        $stats->setName($this->parameters->getStatName());
+
+        foreach ($this->userMonthlyPosts as $user => $postsPerMonth) {
+            $monthPostsTotal = array_sum($postsPerMonth);
+            $monthPostsCount = count($postsPerMonth);
+
+            // Get user posts monthly average round to 2 precision
+            $monthlyAverage = round(($monthPostsTotal / $monthPostsCount), 0, PHP_ROUND_HALF_DOWN);
+            $child = (new StatisticsTo())
+                ->setName($this->parameters->getStatName())
+                ->setSplitPeriod($user)
+                ->setValue($monthlyAverage)
+                ->setUnits(self::UNITS);
+
+            $stats->addChild($child);
+        }
+
+        return $stats;
     }
 }
